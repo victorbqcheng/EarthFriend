6fc3a23e08b98f6b674c63ef280148883afa46ea
victor <3074146158@qq.com>
2023-10-08 10:24:59 +1300

    implement news page

---
+++ components/NewsItem.js
@@ -0,0 +1,46 @@
+import { Dimensions, Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+
+
+const NewsItem = ({data})=>{
+    return (
+        <TouchableOpacity>
+            <View style={styles.container}>
+                <View style={{display:'flex', flex:8}}>
+                    <View style={{display:'flex', flexDirection:'row'}}>
+                        <Image style={{width:0, height:0}}></Image>
+                        <Text style={{fontWeight:'bold', margin:2, fontSize:10}}>{data.author}</Text>
+                    </View>
+                    <Text style={{margin:2}}>{data.title}</Text>
+                </View>
+                <View style={{display:'flex', flex:2}}>
+                    <Image source={{uri:data.imageUrl}} style={styles.img}></Image>
+                </View>
+                
+                {/* <Text>aaaa</Text> */}
+            </View>
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        width:'100%',
+        height: Dimensions.get('window').width * 0.218,
+        flexDirection:'row',
+        justifyContent: 'flex-start',
+        backgroundColor: 'white',
+        alignItems:'center',
+        borderColor: '#ccc',
+      },
+      img:{
+        width: Dimensions.get('window').width * 0.178,
+        height: Dimensions.get('window').width * 0.178,
+        margin: Dimensions.get('window').width * 0.01,
+        borderRadius:10
+      }
+});
+
+export default NewsItem;
+
